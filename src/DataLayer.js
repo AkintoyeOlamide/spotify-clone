cb70af775c2185748e92da1926407eb347062f4d
Akintoye Olamide <Akintoyeolamide500@gmail.com>
2022-04-30 08:23:34 +0800

    working the files for the player

---
+++ src/DataLayer.js
@@ -0,0 +1,14 @@
+import React, 
+{ createContext,
+    useContext,
+    useReducer }
+    from "react";
+
+    export const DataLayerContext = createContext();
+
+    export const DataLayer = ({ initialState, reducer, children}) => (
+        <DataLayerContext.provider value={useReducer(reducer, initialState)}>
+        {children}
+        </DataLayerContext.provider>
+
+    )
--- src/Login.js
+++ src/Login.js
@@ -1,17 +1,17 @@
 import React from "react";
-import  "./Login.css";
-import { loginUrl } from "./spotify";
+import "./Login.css";
+import { accessUrl } from "./spotify";
 
 function Login() {
   return (
     <div className="login">
-        <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png"></img>
-{/* spotify logo */}
-
-<a href={ loginUrl }>LOGIN WITH SPOTIFY</a>
-{/* spotify button */}
+      <img
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+      <a href={accessUrl}>LOGIN TO SPOTIFY</a>
     </div>
-  )
+  );
 }
 
 export default Login;
